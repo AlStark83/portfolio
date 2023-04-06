48d6a98bcdc59ea47d7376575fcdd57eed5988cb
Alberto Guerrero Covarrubias <a.guerrero.c.83@gmail.com>
2023-04-05 23:55:45 -0600

    Hero and NavBar ready whit Css and Info

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,79 +1,11 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import NavBar from "./Components/NavBar.jsx";
-import CardSkills from "./Components/Card_Skills";
-import CardProjects from "./Components/Card_Projects";
+import Home from "./View/Home";
+import "./App.css"
 
 function App() {
-	const frontEnd = CardSkills("Front-End", [
-		"HTML",
-		"CSS",
-		"Javascript",
-		"React",
-		"Jquery",
-		"Oauth",
-	]);
-	const backEnd = CardSkills("Back-End", [
-		"Node.js",
-		"Express.js",
-		"Sequelize",
-		"Postgres",
-		"Restful API's",
-	]);
-	const other = CardSkills("Other", [
-		"Git",
-		"Github",
-		"VS Code",
-		"Scrum Board",
-	]);
-	const basicProject = CardProjects("", "API-Pokemon", "", ["React", "CSS modules", "Redux", "JavaScript", "Sequelize", "Postgres", "Node.js", "Express.js"], )
-
-	return (
-		<div className="App">
-			<div>
-				<NavBar />
-				<h1>Hi There! I'm Al</h1>
-				<h2>Full Stack Developer based in CDMX</h2>
-				<img
-					src="https://www.canva.com/design/DAFdY5LynsA/Lo93P7LhaPTwUiyKym6RzQ/watch?utm_content=DAFdY5LynsA&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink"
-					alt="Me smiling"
-				/>
-			</div>
-			<hr />
-			<div>
-				<div>
-					<h2 id="aboutMe">About Me</h2>
-				</div>
-				<p>
-					Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit
-					nemo, atque incidunt, et voluptates, amet alias doloribus deleniti
-					quae dignissimos vitae praesentium inventore? Recusandae modi incidunt
-					laudantium quo nostrum nisi!
-				</p>
-			</div>
-			<hr />
-			<div>
-				<div>
-					<h2 id="skills">Skills</h2>
-				</div>
-				<div>
-					<div>{frontEnd}</div>
-					<div>{backEnd}</div>
-					<div>{other}</div>
-				</div>
-			</div>
-			<div>
-				<div>
-					<h2 id="projects">My Projects</h2>
-				</div>
-				<div>
-
-				</div>
-			</div>
-		</div>
-	);
+	return( 
+	<Home />
+	)
 }
 
 export default App;
---
+++ portfolio/src/View/Home.jsx
@@ -0,0 +1,100 @@
+import { useState } from "react";
+import NavBar from "../Components/NavBar/NavBar.jsx";
+import CardSkills from "../Components/Card_Skills/Card_Skills.jsx";
+import CardProjects from "../Components/Card_Project/Card_Projects.jsx";
+import styles from "./Home.module.css";
+
+function Home() {
+	const frontEnd = CardSkills("Front-End", [
+		"HTML",
+		"CSS",
+		"Javascript",
+		"React",
+		"Jquery",
+		"Oauth",
+	]);
+	const backEnd = CardSkills("Back-End", [
+		"Node.js",
+		"Express.js",
+		"Sequelize",
+		"Postgres",
+		"Restful API's",
+	]);
+	const other = CardSkills("Other", [
+		"Git",
+		"Github",
+		"VS Code",
+		"Scrum Board",
+	]);
+	const basicProject = CardProjects("", "API-Pokemon", "", [
+		"React",
+		"CSS modules",
+		"Redux",
+		"JavaScript",
+		"Sequelize",
+		"Postgres",
+		"Node.js",
+		"Express.js",
+	]);
+
+	return (
+		<div className={styles.homeContainer}>
+			<div>
+				<div className={styles.header}>
+					<img
+						className={styles.logo}
+						src="src\assets\Alberto Logo (white 320 × 80 px).png"
+						alt="logo"
+					/>
+					<NavBar />
+				</div>
+				<div className={styles.heroContainer}>
+					<div className={styles.hero}>
+						<h1>Hi There! I'm Al</h1>
+						<h2>Full Stack Developer based in CDMX</h2>
+						<div className={styles.imgMe}>
+							<img
+								src="\src\assets\Profile Picture AGC - Editado.png"
+								width="270px"
+								alt="Me smiling"
+							/>
+						</div>
+					</div>
+				</div>
+			</div>
+			<hr />
+			<div className={styles.aboutMeContainer}>
+				<div>
+					<h2 className={styles.aboutMe} id="aboutMe">
+						About Me
+					</h2>
+				</div>
+				<p>
+					Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit
+					nemo, atque incidunt, et voluptates, amet alias doloribus deleniti
+					quae dignissimos vitae praesentium inventore? Recusandae modi incidunt
+					laudantium quo nostrum nisi!
+				</p>
+			</div>
+			<hr />
+			<div>
+				<div>
+					<h2 id="skills">Skills</h2>
+				</div>
+				<div>
+					<div>{frontEnd}</div>
+					<div>{backEnd}</div>
+					<div>{other}</div>
+				</div>
+			</div>
+			<div>
+				<div>
+					<h2 id="projects">My Projects</h2>
+				</div>
+				<div></div>
+			</div>
+		</div>
+	);
+}
+
+export default Home;
